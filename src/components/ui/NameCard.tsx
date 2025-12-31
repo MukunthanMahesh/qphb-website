f54ba64d2507c7c2b3b2bd6c00701d35bfc5dfff
Francesca Adaobi <22kxpt@queensu.ca>
2025-12-31 03:45:53 -0500

    Full teams page draft 1

--- src/components/ui/NameCard.tsx
+++ src/components/ui/NameCard.tsx
@@ -11,7 +11,7 @@ interface NameCardProps {
 
 export function NameCard({name, role, program, imagesrc} : NameCardProps) {
     return(
-       <div className = "h-auto w-65 rounded-2xl border border-white-10 p-0.5 shadow-sm backdrop-blur bg-[conic-gradient(at_center,_orange_0%,_#fff7ee_25%,_orange_50%,_#fff7ee_75%,_orange_100%)] ">
+       <div className      = "h-auto w-65 rounded-2xl border border-white-10 p-0.5 shadow-sm backdrop-blur bg-[conic-gradient(at_center,_orange_0%,_#fff7ee_25%,_orange_50%,_#fff7ee_75%,_orange_100%)] ">
             <div className = "pt-6 pb-3 h-full w-full rounded-2xl border border-black/10 bg-white align-item-center">
                 {/* Image */}
                     <div className="mx-auto h-40 w-40 overflow-hidden rounded-full relative ring-1 ring-[#FFD8C3] rounded-2xl">
