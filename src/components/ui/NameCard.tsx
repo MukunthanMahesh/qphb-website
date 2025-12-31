591bafa8e2d3abd19fb79c12b8783b870324e010
Francesca Adaobi <22kxpt@queensu.ca>
2025-12-31 03:45:53 -0500

    Full teams page draft 1

---
+++ src/components/ui/NameCard.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+
+interface NameCardProps {
+  name: string;
+  role: string;
+  program: string;
+  imagesrc: string;
+}
+
+
+export function NameCard({name, role, program, imagesrc} : NameCardProps) {
+    return(
+       <div className = "h-auto w-65 rounded-2xl border border-white-10 p-0.5 shadow-sm backdrop-blur bg-[conic-gradient(at_center,_orange_0%,_#fff7ee_25%,_orange_50%,_#fff7ee_75%,_orange_100%)] ">
+            <div className = "pt-6 pb-3 h-full w-full rounded-2xl border border-black/10 bg-white align-item-center">
+                {/* Image */}
+                    <div className="mx-auto h-40 w-40 overflow-hidden rounded-full relative ring-1 ring-[#FFD8C3] rounded-2xl">
+                        <img
+                            src={imagesrc}
+                            alt= {'${name} headshot'}
+                        />
+                    </div>
+                {/* Text Info */}
+                <div className="p-3">
+                    <p className="font-normal text-sm leading-relaxed text-foreground/100 sm:text-base text-center">
+                    {name}
+                    </p>
+
+                    <p className="font-bold text-sm leading-relaxed text-[#C76604]/90 sm:text-base text-center">
+                    {role}
+                    </p>
+
+                    <p className="font-normal text-sm text-gray-500 text-center">
+                    {program}
+                    </p>
+                </div>
+            </div>
+       </div>
+    );
+}
