56d0280e589b044d69c4df7d22fb6b0d05b6abd5
Akshaen <146366203+Akshaen@users.noreply.github.com>
2026-01-15 17:34:35 -0500

    Create the other sections of About us page

--- src/components/sections/about/AboutPage.tsx
+++ src/components/sections/about/AboutPage.tsx
@@ -1,9 +1,15 @@
 import { AboutHero } from "./AboutHero"
+import { AboutPillars } from "./AboutPillars"
+import { AboutStory } from "./AboutStory"
+import { AboutRoadMap } from "./AboutRoadMap"
 
-export default function TeamPage() {
+export default function AboutPage() {
   return (
       <>
       <AboutHero />
+      <AboutPillars />
+      <AboutStory />
+      <AboutRoadMap />
 
       </>
     )
---
+++ src/components/sections/about/AboutRoadMap.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { PagePlaceholder } from "@/components/ui/PagePlaceholder"
+
+export function AboutRoadMap() {
+  return (
+    <PagePlaceholder
+      title="About Us"
+      description="Learn more about QPHB’s mission, values, and initiatives."
+    />
+  )
+}
