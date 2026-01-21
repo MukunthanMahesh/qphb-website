d6bcb1c2b43ae0a85d354f9721ace9ea1b78af06
Akshaen <146366203+Akshaen@users.noreply.github.com>
2026-01-21 01:11:42 -0500

    Update AboutPage.tsx

--- src/components/sections/about/AboutPage.tsx
+++ src/components/sections/about/AboutPage.tsx
@@ -2,6 +2,7 @@ import { AboutHero } from "./AboutHero"
 import { AboutPillars } from "./AboutPillars"
 import { AboutStory } from "./AboutStory"
 import { AboutRoadMap } from "./AboutRoadMap"
+import {AboutJoinUs} from "./AboutJoinUs"
 
 export default function AboutPage() {
   return (
@@ -10,6 +11,7 @@ export default function AboutPage() {
       <AboutPillars />
       <AboutStory />
       <AboutRoadMap />
+      <AboutJoinUs />
 
       </>
     )
